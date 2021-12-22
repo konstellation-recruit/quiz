f6baa7cc1a3f273969189bd2442d1cce0498f91d
zsef123 <zsef123@gmail.com>
2021-12-22 16:22:56 +0900

    fix send error, add msg_type

--- quiz_backend/start_practice_quiz.py
+++ quiz_backend/start_practice_quiz.py
@@ -32,26 +32,8 @@ questions = [
 
 async def send_user_ox(ws, q_id):
     await ws.send(json.dumps({
-        "type": "submit",
-        "user_id": 1,
-        "q_id": q_id,
-        "select": 'o' # select is must be 'o' or 'x'
-    }))
-    await ws.recv() # is this needed?
-    await asyncio.sleep(1)
-
-    await ws.send(json.dumps({
-        "type": "submit",
-        "user_id": 1,
-        "q_id": q_id,
-        "select": 'x' # select is must be 'o' or 'x'
-    }))
-    await ws.recv() # is this needed?
-    await asyncio.sleep(1)
-
-    await ws.send(json.dumps({
-        "type": "submit",
-        "user_id": "sdf@svn.com", # changed to mail
+        "msg_type": "submit",
+        "user_id": "qwer@svn.com",
         "name": "foo",
         "q_id": q_id,
         "select": 'o' # select is must be 'o' or 'x'
@@ -60,8 +42,29 @@ async def send_user_ox(ws, q_id):
     await asyncio.sleep(1)
 
     await ws.send(json.dumps({
-        "type": "submit",
-        "user_id": 2,
+        "msg_type": "submit",
+        "user_id": "qwer@svn.com",
+        "name": "foo",
+        "q_id": q_id,
+        "select": 'x' # select is must be 'o' or 'x'
+    }))
+    await ws.recv() # is this needed?
+    await asyncio.sleep(1)
+
+    await ws.send(json.dumps({
+        "msg_type": "submit",
+        "user_id": "asdf@svn.com",
+        "name": "bar",
+        "q_id": q_id,
+        "select": 'o' # select is must be 'o' or 'x'
+    }))
+    await ws.recv() # is this needed?
+    await asyncio.sleep(1)
+
+    await ws.send(json.dumps({
+        "msg_type": "submit",
+        "user_id": "asdf@svn.com",
+        "name": "bar",
         "q_id": q_id,
         "select": 'o' # select is must be 'o' or 'x'
     }))
@@ -69,23 +72,31 @@ async def send_user_ox(ws, q_id):
     await asyncio.sleep(1)
 
 
-async def send_question(ws, question):
+async def create_question(ws, question):
     create_q_time = time.time()
-    data = {"type": "question", **question, "create_time": create_q_time}
+    data = {"msg_type": "create_question", **question, "create_time": create_q_time}
     await ws.send(json.dumps(data))
     await ws.recv() # is this needed?
     await asyncio.sleep(1)
     return create_q_time
+
+async def show_answer(ws, q_id):
+    data = {"msg_type": "show_answer", "q_id": q_id}
+    await ws.send(json.dumps(data))
+    await ws.recv() # is this needed?
+    await asyncio.sleep(5)
 
 
 async def main():
     submit_time = 10 # sec
     async with websockets.connect("ws://localhost:8000/ws/quiz/") as ws:
         for q in questions:
-            create_q_time = await send_question(ws, q)
+            create_q_time = await create_question(ws, q)
 
-            while (time.time() - create_q_time <= submit_time):
-                await send_user_ox(ws, q["number"])
+            # while (time.time() - create_q_time <= submit_time):
+            await send_user_ox(ws, q["number"])
+
+            await show_answer(ws, q["number"])
 
 
 asyncio.run(main())
