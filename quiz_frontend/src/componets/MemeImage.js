2ec9d2c68651d92f7573833bad86054cddaef3a5
leedc0101 <leedc0101@naver.com>
2021-12-22 22:44:00 +0900

    Change Layout

--- quiz_frontend/src/componets/MemeImage.js
+++ quiz_frontend/src/componets/MemeImage.js
@@ -5,7 +5,7 @@ export default function MemeImage() {
     const url = useSelector((state) => state.image);
 
     return (
-        <VStack p={5} boxShadow="dark-lg" borderRadius={20} justify="center" align="center" w="400px" h="350px" bg="#252d4a">
+        <VStack p={5} boxShadow="dark-lg" borderRadius={20} justify="center" align="center" w="400px" minH="350px" bg="#252d4a">
             <Image src={url} />
         </VStack>
     );
